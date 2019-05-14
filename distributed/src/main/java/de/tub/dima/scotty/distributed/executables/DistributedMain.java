6d35612c3952ecf7f8494eabd232a400dabb4708
Lawrence <mail@lawben.com>
2019-05-14 12:01:14 +0200

    Extract tshark runner

--- distributed/src/main/java/de/tub/dima/scotty/distributed/executables/DistributedMain.java
+++ distributed/src/main/java/de/tub/dima/scotty/distributed/executables/DistributedMain.java
@@ -1,24 +1,10 @@
 package de.tub.dima.scotty.distributed.executables;
 
-import de.tub.dima.scotty.distributed.DistributedChild;
 import de.tub.dima.scotty.distributed.DistributedUtils;
-import de.tub.dima.scotty.distributed.InputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.Instant;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DistributedMain {
-    // Example invocation:
-    // tshark -f \"tcp port 4055 || tcp port 4056 || tcp portrange "4060-4069 || tcp portrange 4160-4169\"
-    // -i lo0 -w /tmp/single-run-4-20-1000000.pcap (-a duration:300)
-    private static final String TSHARK_PORT_TEMPLATE = "tcp port %d || tcp port %d || tcp portrange %d-%d || tcp portrange %d-%d";
-
-
     public static void main(String[] args) throws Exception {
         if (args.length < 7) {
             System.err.println("Not enough arguments!\nUsage: java ... "
@@ -41,17 +27,7 @@ public class DistributedMain {
         final int numStreams = Integer.parseInt(args[5]);
         final int numEvents = Integer.parseInt(args[6]);
 
-
-        final int streamRegisterPort = streamPort + DistributedChild.STREAM_REGISTER_PORT_OFFSET;
-        final String networkInterface = "lo0";
-        Instant runTimestamp = Instant.ofEpochMilli(System.currentTimeMillis());
-        final String tsharkOutputFile = String.format("/Users/law/repos/ma/runs-scotty/dist-run-%d-%d-%d-%s.pcap", numChildren, numStreams, numEvents, runTimestamp);
-
-        String tsharkPortString = String.format(TSHARK_PORT_TEMPLATE, rootControllerPort, rootWindowPort, streamPort,
-                                                streamPort + 10, streamRegisterPort, streamRegisterPort + 10);
-        String[] tsharkArgs = {"tshark", "-f", tsharkPortString, "-i", networkInterface, "-w", tsharkOutputFile};
-        System.out.println("Starting tshark with command:\n\t" + Arrays.toString(tsharkArgs));
-        Process tshark = Runtime.getRuntime().exec(tsharkArgs);
+        Process tshark = TsharkRunner.startTshark(rootControllerPort, rootWindowPort, streamPort, numChildren, numStreams, numEvents);
 
         System.out.println("Running with " + numChildren + " children, " + numStreams + " streams, and " +
                 numEvents + " events per stream.");
@@ -79,17 +55,6 @@ public class DistributedMain {
 
         rootThread.join();
         System.out.println("Finished streaming.");
-        System.out.println("TSHARK OUT:\n-----------");
-        printTsharkOut(tshark.getErrorStream());
-        printTsharkOut(tshark.getInputStream());
-        tshark.destroy();
+        TsharkRunner.endTshark(tshark);
     }
-
-    private static void printTsharkOut(java.io.InputStream in) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(in));
-        while (input.ready()) {
-            System.out.println(input.readLine());
-        }
-    }
-
 }
--- distributed/src/main/java/de/tub/dima/scotty/distributed/executables/single/SingleMain.java
+++ distributed/src/main/java/de/tub/dima/scotty/distributed/executables/single/SingleMain.java
@@ -1,23 +1,13 @@
 package de.tub.dima.scotty.distributed.executables.single;
 
-import de.tub.dima.scotty.distributed.DistributedChild;
 import de.tub.dima.scotty.distributed.DistributedUtils;
 import de.tub.dima.scotty.distributed.executables.InputStreamMain;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.Instant;
-import java.util.Arrays;
+import de.tub.dima.scotty.distributed.executables.TsharkRunner;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SingleMain {
-    // Example invocation:
-    // tshark -f \"tcp port 4055 || tcp port 4056 || tcp portrange "4060-4069 || tcp portrange 4160-4169\"
-    // -i lo0 -w /tmp/single-run-4-20-1000000.pcap (-a duration:300)
-    private static final String TSHARK_PORT_TEMPLATE = "tcp port %d || tcp port %d || tcp portrange %d-%d || tcp portrange %d-%d";
-
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
         if (args.length < 6) {
             System.out.println("Not enough arguments!\nUsage: java ... "
                     + "rootPort "
@@ -37,18 +27,7 @@ public class SingleMain {
         final int numStreams = Integer.parseInt(args[4]);
         final int numEvents = Integer.parseInt(args[5]);
 
-
-        final int streamRegisterPort = streamPort + DistributedChild.STREAM_REGISTER_PORT_OFFSET;
-        final String networkInterface = "lo0";
-        Instant runTimestamp = Instant.ofEpochMilli(System.currentTimeMillis());
-        final String tsharkOutputFile = String.format("/Users/law/repos/ma/runs-scotty/dist-run-%d-%d-%d-%s.pcap", numChildren, numStreams, numEvents, runTimestamp);
-
-        String tsharkPortString = String.format(TSHARK_PORT_TEMPLATE, rootPort, rootPort, streamPort,
-                streamPort + 10, streamRegisterPort, streamRegisterPort + 10);
-        String[] tsharkArgs = {"tshark", "-f", tsharkPortString, "-i", networkInterface, "-w", tsharkOutputFile};
-        System.out.println("Starting tshark with command:\n\t" + Arrays.toString(tsharkArgs));
-        Process tshark = Runtime.getRuntime().exec(tsharkArgs);
-
+        Process tshark = TsharkRunner.startTshark(rootPort, rootPort, streamPort, numChildren, numStreams, numEvents);
 
         System.out.println("Running with " + numChildren + " children, " + numStreams + " streams, and " +
                 numEvents + " events per stream.");
@@ -77,16 +56,6 @@ public class SingleMain {
 
         rootThread.join();
         System.out.println("Finished streaming.");
-        System.out.println("TSHARK OUT:\n-----------");
-        printTsharkOut(tshark.getErrorStream());
-        printTsharkOut(tshark.getInputStream());
-        tshark.destroy();
-    }
-
-    private static void printTsharkOut(java.io.InputStream in) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(in));
-        while (input.ready()) {
-            System.out.println(input.readLine());
-        }
+        TsharkRunner.endTshark(tshark);
     }
 }
