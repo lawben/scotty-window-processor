6cf1a7309923a122633bd8e0bd42ed26a523c456
Lawrence Benson <github@lawben.com>
2019-05-21 11:23:43 +0200

    Update init.sh

--- distributed/init.sh
+++ distributed/init.sh
@@ -13,5 +13,6 @@ cd distributed-scotty
 
 mvn clean install -pl :distributed -am
 
-export CLASSPATH="$HOME_DIR/distributed-scotty/distributed/target/classes:$HOME_DIR/distributed-scotty/core/target/classes:$HOME_DIR/distributed-scotty/state/target/classes:$HOME_DIR/distributed-scotty/slicing/target/classes:$HOME_DIR/.m2/repository/org/jetbrains/annotations/17.0.0/annotations-17.0.0.jar:$HOME_DIR/.m2/repository/org/zeromq/jeromq/0.5.1/jeromq-0.5.1.jar:$HOME_DIR/.m2/repository/eu/neilalexander/jnacl/1.0.0/jnacl-1.0.0.jar:$HOME_DIR/.m2/repository/org/slf4j/slf4j-api/1.7.16/slf4j-api-1.7.16.jar"
-echo "CLASSPATH=$CLASSPATH" >> ${HOME_DIR}/.bashrc
+export CLASSPATH="$HOME_DIR/distributed-scotty:$HOME_DIR/distributed-scotty/distributed/target/classes:$HOME_DIR/distributed-scotty/core/target/classes:$HOME_DIR/distributed-scotty/state/target/classes:$HOME_DIR/distributed-scotty/slicing/target/classes:$HOME_DIR/.m2/repository/org/jetbrains/annotations/17.0.0/annotations-17.0.0.jar:$HOME_DIR/.m2/repository/org/zeromq/jeromq/0.5.1/jeromq-0.5.1.jar:$HOME_DIR/.m2/repository/eu/neilalexander/jnacl/1.0.0/jnacl-1.0.0.jar:$HOME_DIR/.m2/repository/org/slf4j/slf4j-api/1.7.16/slf4j-api-1.7.16.jar"
+echo "export CLASSPATH=$CLASSPATH" >> ${HOME_DIR}/.bashrc
+source "$HOME_DIR/.bashrc"
