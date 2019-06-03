2254a2adfcd7e3071188d26d4e1fa1503363d3a2
Lawrence <github@lawben.com>
2019-06-03 14:05:35 +0200

    Remove distributed

--- distributed/src/main/java/de/tub/dima/scotty/distributed/EventGenerator.java
+++
@@ -1,8 +0,0 @@
-package de.tub.dima.scotty.distributed;
-
-import java.util.Random;
-import org.zeromq.ZMQ;
-
-public interface EventGenerator<T> {
-    long generateAndSendEvents(Random rand, ZMQ.Socket eventSender) throws Exception;
-}
--- distributed/src/test/java/de/tub/dima/scotty/distributed/DistributedWindowTest.java
+++
@@ -1,9 +0,0 @@
-package de.tub.dima.scotty.distributed;
-
-import org.junit.Test;
-
-public class DistributedWindowTest {
-    @Test
-    public void testX() {
-    }
-}
